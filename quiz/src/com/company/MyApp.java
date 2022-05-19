f0f15895a24877c849bfd56e00f9fc64772810a2
Elma Skopljak <105375038+Elmaaaaaaa@users.noreply.github.com>
2022-05-19 17:42:49 +0200

    Add files via upload

---
+++ quiz/src/com/company/MyApp.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+        System.out.println("CHECK YOUR KNOWLEDGE- CAPITAL CITIES\n\n");
+	String firstQuestion = "What is the capital city of Spain?\n " +
+            "(a) Barcelona\n (b) Córdoba\n (c) Madrid ";
+        String secondQuestion = "What is the capital city of Albania?\n " +
+                "(a) Tirana\n (b) Finiq\n (c) Klos";
+        String thirdQuestion = "What is the capital city of Island?\n " +
+                "(a) Akranes\n (b) Akureyri\n (c) Reykjavik ";
+        String fourthQuestion = "What is the capital city of Switzerland?\n " +
+                "(a) Zürich\n (b) Bern\n (c) Basel ";
+        String fifthQuestion = "What is the capital city of Malta?\n " +
+                "(a) Valletta\n (b) Il-Birgu \n (c) Ir-Rabat ";
+        //create an array with question elements(objects)
+        Questions[] questions = {new Questions(firstQuestion, "c"),
+        new Questions(secondQuestion, "a"), new Questions(thirdQuestion, "c"),
+        new Questions(fourthQuestion, "b"), new Questions(fifthQuestion, "a")};
+        takeTest(questions);
+
+    }
+    public static void takeTest(Questions[] questions){
+        int score = 0;
+        for(int i = 0; i < questions.length; i++){
+            System.out.println(questions[i].prompt);
+            Scanner input = new Scanner(System.in);
+            String userAnswer = input.nextLine();
+            if(userAnswer.equals(questions[i].answer)){
+                score++;
+            }
+
+
+        }
+        System.out.println("Your score is: " + score + "/" + questions.length);
+
+    }
+}
---
+++ quiz/src/com/company/Questions.java
@@ -0,0 +1,12 @@
+package com.company;
+//create class Questions
+public class Questions {
+    String prompt;
+    String answer;
+    //create constructor- init method
+    public Questions(String prompt, String answer){
+        this.prompt = prompt;
+        this.answer = answer;
+
+    }
+}
